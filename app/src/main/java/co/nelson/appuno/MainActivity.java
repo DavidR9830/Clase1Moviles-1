0856db5424b356e0acc4ae77b995593ab86c15b4
Miguel Angel Vallejo Giraldo <giraldomiguel21@gmail.com>
2024-02-08 09:29:52 -0500

    Clase 2
    Metodos + Strings + Colores + Botones

--- app/src/main/java/co/nelson/appuno/MainActivity.java
+++ app/src/main/java/co/nelson/appuno/MainActivity.java
@@ -6,14 +6,39 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
-    boolean result = true;
-    String name = "Bob";
+    TextView nombreUsuario;
+    Button btn1, btn2;
+
+    /* 
+    boolean result = false;
+    String name = "Triana";
+    */
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
 
+            nombreUsuario = /*(TextView)*/ findViewById(R.id.usuario);
+            btn1 = /*(Button)*/ findViewById(R.id.btn1);
+            btn2 = /*(Button)*/ findViewById(R.id.btn2);
 
+            btn1.setOnClickListener(new View.OnClickListener(){
+                @Override
+                public void onClick(View view){
+                    nombreUsuario.setText("Triana es gei");
+                }
+            });
+
+            btn2.setOnClickListener(new View.OnClickListener(){
+                @Override
+                public void onClick(View view){
+                    nombreUsuario.setText("Triana es maricon");
+                }
+            });
+    }
+
+    public double calcularArea(int l1, int l2){
+        return l1*l2;
     }
 }
